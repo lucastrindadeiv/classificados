847201750a73f0ebe976fd1e022c70b93f0b81c4
Lucas Trindade <lucastrindadeiv@gmail.com>
2019-04-12 19:26:51 -0300

    Navbar, Jumbotron, Require

---
+++ classes/header.php
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+	<title>Classificados</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="./" class="navbar-brand">Classificados</a>
+		</div>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="">Cadastre-se</a></li>
+			<li><a href="">Login</a></li> 
+		</ul>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php require 'pages/header.php'; ?>
+
+	<div class="container-fluid">
+		<div class="jumbotron">
+	   		<h2> Nós temos hoje 999 anúncios. </h2>
+	   		<p>E mais de 999 usuários cadastrados.</p>
+		</div>	
+
+		<div class="row">
+			<div class="col-sm-3">
+				<h4>Pesquisa Avançada</h4>
+			</div>
+			<div class="col-sm-9">
+				<h4>Últimos Anúncios</h4>
+			</div>
+		</div>
+
+	</div>
+
+<?php require 'pages/footer.php'; ?>
